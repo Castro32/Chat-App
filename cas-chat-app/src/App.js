e7b1bd16dbbb82c7db4b0830989eec0b794dfee9
Castro32 <okanga.fidel@gmail.com>
2023-12-18 14:59:36 +0300

    created a new chat app using react

---
+++ cas-chat-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="container">
+      <section className='secContainer'>
+        <Navbar/>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ cas-chat-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css';
+
+const Navbar = () =>{
+    return (
+        <div className='Navbar'>
+        <div className='bar'>
+            <img src='./chat.jpg' alt='logo'/>
+        </div>
+        <div className='nav'>
+            <img src='./menu-bar.png' alt='menu-bar' />
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
