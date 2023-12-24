f8c22081ec364156e21162f9a63496a533bb68da
Castro32 <okanga.fidel@gmail.com>
2023-12-24 21:26:29 +0300

    added a forgot password page

---
+++ cas-chat-app/src/components/ForgotPassword/ForgotPassword.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './ForgotPassword.css';
+const ForgotPassword=()=>{
+  const [action, setAction] = useState('Sign Up');
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+    return(
+        <div className='container'>
+          <div className='input'>
+          <img src='./mail_646135.png' alt='Email' />
+          <input type='email' placeholder='example@gmail.com' />
+          </div>
+
+          <div className='input'>
+          <img src='./password_159478.png' alt='Password' />
+          <input
+            type={showPassword ? 'text' : 'password'}
+            placeholder='Password'/>
+          </div>
+          <div className='password-toggle-icon' onClick={togglePasswordVisibility}>
+            {showPassword ? (
+              <img src='./eyes_5904983.png' alt='Hide Password' />
+            ) : (
+              <img src='./view_402758.png' alt='Show Password' />
+            )}
+          </div>
+        </div>
+    );
+            }
+
+
+export default ForgotPassword
