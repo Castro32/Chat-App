78a2898d2304f2019d0443ab02b775d21227a093
Castro32 <okanga.fidel@gmail.com>
2023-12-30 22:25:42 +0300

    updated the sign in page

--- cas-chat-app/src/App.js
+++ cas-chat-app/src/App.js
@@ -1,21 +1,23 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {useAuthState} from 'react-firebase-hooks/auth'
-import firebase from './firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebase';
 import SignIn from './components/SignIn/SignIn';
 import ForgotPassword from './components/ForgotPassword/ForgotPassword';
 
-
 function App() {
+  const [user] = useAuthState(firebase.auth());
+
   return (
-    <div className="contaner">
+    <div className="container">
       <section className='secContainer'>
-        <Navbar/>
-        <SignIn/>
-        <ForgotPassword/>
+        {!user && <SignIn />} {/* Show SignIn only when user is not logged in */}
+        {!user && <ForgotPassword />} {/* Show ForgotPassword only when user is not logged in */}
       </section>
     </div>
   );
 }
+
+
 export default App;
